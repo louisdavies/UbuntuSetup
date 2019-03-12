bca278b1e94a75ab6a44d2513a69ded35440f6fc
louisdavies <37486597+louisdavies@users.noreply.github.com>
2019-03-12 10:37:31 +0000

    Fixed bug which gives malformed source list entry preventing apt commands.

--- preferences.sh
+++ preferences.sh
@@ -19,7 +19,7 @@ sudo sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop
 
 # Install Sublime
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
-echo "deb https://download.sublimetext.com/apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+echo "deb https://download.sublimetext.com/ apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
 sudo apt-get update
 sudo apt-get install sublime-text
 
