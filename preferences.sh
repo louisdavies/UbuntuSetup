247b541b15305e8341c67e667c1c1298e3fdcb8d
Louis Davies <louis.davies@hotmail.com>
2019-08-17 20:11:47 +0100

    Added recent improvements

--- preferences.sh
+++ preferences.sh
@@ -29,3 +29,36 @@ sudo sensors-detect
 # TODO type (y) a lot of times
 sensors
 
+# Not sure if I want preload, but probably do
+sudo apt-get install preload
+systemctl status preload
+
+# Increase threshold for virtual RAM useage (Threshold = (100-vm.swappiness)%)
+echo "vm.swappiness=10" | sudo tee /etc/sysctl.d/99-sysctl.conf 
+sudo sysctl vm.swappiness=10
+
+# Allow startup applications to be seen in "Startup Applications"
+sudo sed -i "s/NoDisplay=true/NoDisplay=false/g" /etc/xdg/autostart/*.desktop
+
+# Install Sublime
+wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | sudo apt-key add -
+echo "deb https://download.sublimetext.com/apt/stable/" | sudo tee /etc/apt/sources.list.d/sublime-text.list
+sudo apt-get update
+sudo apt-get install sublime-text
+
+# To try out:
+dconf dump /org/compiz/profiles/unity/ > ccsm.cfg
+dconf load /org/compiz/profiles/unity/ < ccsm.cfg
+http://www.florian-diesch.de/software/unsettings/	
+
+#LTSpice
+sudo dpkg --add-architecture i386             # Add the i386 architecture
+sudo apt-get update
+sudo apt-get install wine wine-mono0.0.8 wine-gecko2.2
+
+unistall lubuntu
+
+copy over .bashrc
+sudo apt-get install git
+mkdir ~/UbuntuSetup
+git pull
