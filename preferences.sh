13d2af2ce15dc82946c1bec0edd5eaa8b24722c4
louisdavies <37486597+louisdavies@users.noreply.github.com>
2019-09-05 13:43:19 +0100

    Update preferences.sh
    Added Python3.7 install steps

--- preferences.sh
+++ preferences.sh
@@ -62,3 +62,17 @@ copy over .bashrc
 sudo apt-get install git
 mkdir ~/UbuntuSetup
 git pull
+
+# Get Python3.7
+sudo apt-get install build-essential checkinstall
+sudo apt-get install libreadline-gplv2-dev libncursesw5-dev libssl-dev \
+    libsqlite3-dev tk-dev libgdbm-dev libc6-dev libbz2-dev libffi-dev zlib1g-dev
+# Step 2 – Download Python 3.7
+cd /usr/src
+sudo wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz
+sudo tar xzf Python-3.7.4.tgz
+
+# Step 3 – Compile Python Source
+cd Python-3.7.4
+sudo ./configure --enable-optimizations
+sudo make altinstall
