08130dc8d25160e05c1f68e2b2c7f38b8eddaa2a
louisdavies <37486597+louisdavies@users.noreply.github.com>
2021-01-04 15:19:08 +0000

    Update preferences.sh

--- preferences.sh
+++ preferences.sh
@@ -114,3 +114,13 @@ python scripts/mk_make.py
 cd build
 make -j$(nproc)
 sudo make install
+
+# Boolector
+git clone https://github.com/boolector/boolector
+cd boolector
+./contrib/setup-btor2tools.sh
+./contrib/setup-lingeling.sh
+./configure.sh
+make -C build -j$(nproc)
+sudo cp build/bin/{boolector,btor*} /usr/local/bin/
+sudo cp deps/btor2tools/bin/btorsim /usr/local/bin/
